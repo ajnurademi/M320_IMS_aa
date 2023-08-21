975c94bf8af47acdfbaf556cb0123995af47a975
ajnurademi <ajnur.ademi@hotmail.com>
2023-08-21 10:27:50 +0200

    Projektdateien hinzufügen.

---
+++ M320_IMS_aa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M320_IMS_aa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
