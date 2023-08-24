59e6dfaf72fdcc9fe9583eb54029a3125e552563
ajnurademi <ajnur.ademi@hotmail.com>
2023-08-24 20:05:06 +0200

    Version 1.0

---
+++ OO_Function/MyMath.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_Function
+{
+    public class MyMath
+    {
+        // ggT berechnen
+        public static int Calc_ggT(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        // kgv berechnen
+        public static int Calc_kgV(int a, int b)
+        {
+            int gcd = Calc_ggT(a, b);
+            return (a * b) / gcd;
+        }
+
+        // fragt nach positive Zahl
+        public static int ReadInt(string message)
+        {
+            int number;
+            do
+            {
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out number) || number <= 0);
+            return number;
+        }
+
+        // Zeigt das Resultat an
+        public static void ShowResult(string operation, int a, int b, int result)
+        {
+            Console.WriteLine($"{operation} von {a} und {b} ist {result}");
+        }
+
+        // Array wird umgedreht
+        public static void ReverseArray(int[] numbers)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                int temp = numbers[left];
+                numbers[left] = numbers[right];
+                numbers[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        // Array sortieren
+        public static void SortArray(int[] numbers)
+        {
+            Array.Sort(numbers);
+        }
+
+        //arithmetischer Mittelwer
+        public static double CalculateMean(int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+            double sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum / numbers.Length;
+        }
+
+        // kleinster Wert
+        public static int FindSmallest(int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+            int smallest = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+            }
+            return smallest;
+        }
+
+        // groesster Wert
+        public static int FindLargest(int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+            int largest = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+            return largest;
+        }
+
+        // 2 Variablen vertauscht
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
--- OO_Function/Program.cs
+++ OO_Function/Program.cs
@@ -1,10 +1,104 @@
-﻿namespace OO_Function
+﻿using System;
+
+namespace OO_Function
 {
-    internal class Program
+
+    /*
+    Grundlegende Sprachelemente von C# anzuwenden
+    Autor: Ajnur Ademi
+    Datum: 24.08.2023
+    Version: 1.0
+    */
+
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Eingabe Users solange bis beendet
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("1. ggT berechnen");
+                Console.WriteLine("2. kgV berechnen");
+                Console.WriteLine("3. Statistiken über Zahlen");
+                Console.WriteLine("4. Zahlen umdrehen");
+                Console.WriteLine("5. Zahlen sortieren");
+                Console.WriteLine("6. beenden");
+                Console.Write("Wählen Sie eine Option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                // Eingabe nach Funktion auswerten 
+                switch (choice)
+                {
+                    case 1:
+                        int num1 = MyMath.ReadInt("Geben Sie die erste Zahl ein: ");
+                        int num2 = MyMath.ReadInt("Geben Sie die zweite Zahl ein: ");
+                        int gcd = MyMath.Calc_ggT(num1, num2);
+                        MyMath.ShowResult("ggT", num1, num2, gcd);
+                        break;
+
+                    case 2:
+                        num1 = MyMath.ReadInt("Geben Sie die erste Zahl ein: ");
+                        num2 = MyMath.ReadInt("Geben Sie die zweite Zahl ein: ");
+                        int lcm = MyMath.Calc_kgV(num1, num2);
+                        MyMath.ShowResult("kgV", num1, num2, lcm);
+                        break;
+
+                    case 3:
+                        int arraySize = MyMath.ReadInt("Geben Sie die Anzahl der Zahlen ein: ");
+                        int[] numbers = new int[arraySize];
+                        for (int i = 0; i < arraySize; i++)
+                        {
+                            numbers[i] = MyMath.ReadInt($"Geben Sie Zahl {i + 1} ein: ");
+                        }
+                        Console.WriteLine($"Arithmetischer Mittelwert: {MyMath.CalculateMean(numbers)}");
+                        Console.WriteLine($"Kleinster Wert: {MyMath.FindSmallest(numbers)}");
+                        Console.WriteLine($"Größter Wert: {MyMath.FindLargest(numbers)}");
+                        break;
+
+                    case 4:
+                        arraySize = MyMath.ReadInt("Geben Sie die Anzahl der Zahlen ein: ");
+                        numbers = new int[arraySize];
+                        for (int i = 0; i < arraySize; i++)
+                        {
+                            numbers[i] = MyMath.ReadInt($"Geben Sie Zahl {i + 1} ein: ");
+                        }
+                        MyMath.ReverseArray(numbers);
+                        Console.WriteLine("Zahlen umgedreht:");
+                        foreach (int num in numbers)
+                        {
+                            Console.Write($"{num} ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 5:
+                        arraySize = MyMath.ReadInt("Geben Sie die Anzahl der Zahlen ein: ");
+                        numbers = new int[arraySize];
+                        for (int i = 0; i < arraySize; i++)
+                        {
+                            numbers[i] = MyMath.ReadInt($"Geben Sie Zahl {i + 1} ein: ");
+                        }
+                        MyMath.SortArray(numbers);
+                        Console.WriteLine("Zahlen sortiert:");
+                        foreach (int num in numbers)
+                        {
+                            Console.Write($"{num} ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Danke für Ihr Besuch ! ");
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ungültige Option. Bitte wählen Sie erneut.");
+                        break;
+                }
+            }
         }
     }
 }
